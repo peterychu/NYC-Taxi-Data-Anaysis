9220aac4eeaa5fbf8b0a503621dfdf567e670f61
peterychu <p.chu1330@gmail.com>
2024-06-05 01:59:08 -0700

    Update pipeline_template.py

--- pipeline_template.py
+++ pipeline_template.py
@@ -7,6 +7,18 @@ import itertools
 payment_type_name_map = {1:'Credit Card', 2:'Cash', 3:'No charge', 
                          4:'Dispute', 5:'Unknown',  6:'Voidedtrip'}
 
+
+datetime_dim_schema = '''index:INTEGER, tpep_pickup_datetime:DATETIME, PU_day:INTEGER, PU_month:INTEGER, 
+                             PU_year:INTEGER, tpep_dropoff_datetime:DATETIME '''
+
+payment_type_dim_schema = None
+ratecode_dim_schema = None
+cost_dim_schema = None
+pickup_location_dim_schema = None
+dropoff_location_dim_schema = None
+
+
+
 def create_datetim_dim(row):
     index = row['index']
     tpep_pickup_datetime = row['tpep_pickup_datetime']
@@ -21,7 +33,7 @@ def create_datetim_dim(row):
         
         
 
-    new_row = {'index' : index,
+    new_row = {'datetime_ID' : index,
                 'tpep_pickup_datetime' : tpep_pickup_datetime,
                 'PU_day' : PU_day,
                 'PU_month' : PU_month,
@@ -64,12 +76,42 @@ def create_ratecode_dim(row):
     return new_row
     
 def create_cost_dim(row):
+    cost_ID = row['index']
+    fare_amount = row['fare_amount']
+    tolls_amount = row['tolls_amount']
+    airport_fee = row['Airport_fee']
+    extra = row['extra']
+    improvement_surcharge = row['improvement_surcharge']
+    congestion_surcharge = row['congestion_surcharge']
+    tip_amount = row['tip']
+    total_amount = row['total_amount']
+
+    new_row = {'cost_ID' : cost_ID, 'fare_amount' : fare_amount,
+               'tolls_amount' :  tolls_amount, 'airport_fee' : airport_fee, 
+               'extra' : extra, 'improvement_surcharge' : improvement_surcharge, 
+               'congestion_surcharge' : congestion_surcharge,
+               'tip_amount' : tip_amount, 'total_amount' : total_amount}
+
+    return new_row
+
+def create_initial_pickup_location_dim(row):
+
+    PU_ID = row['index']
+    PU_LocationID = row['PULocationID']
+
+    new_row = {'PU_ID' : PU_ID, 'PU_LocationID' : PU_LocationID}
+
+    return new_row
+
+def create_initial_dropoff_location_dim(row):
+
+    DO_ID = row['index']
+    DO_LocationID = row['DOLocationID']
+
+    new_row = {'DO_ID' : DO_ID, 'DO_LocationID' : DO_LocationID}
+
+    return new_row
      
-    return None
-    
-       
-
-
 def run_pipeline(project_id, bucket_name, input_path, output_table):
     
     options = beam.pipeline.PipelineOptions(
@@ -82,12 +124,11 @@ def run_pipeline(project_id, bucket_name, input_path, output_table):
     with beam.Pipeline(options=options) as p:
     # Read parquet data
         data = (p | 'ReadParquet' >> beam.io.ReadFromParquet(f'gs://{bucket_name}/{input_path}/*.parquet'))
-
+        TaxiZoneLookup = (p | 'ReadParquet' >> beam.io.ReadFromCsv(f'gs://{bucket_name}/{input_path}/*.csv'))
     # split_data = data | 'SplitName' >> beam.Map(split_name)
     # Define BigQuery schema
     # table_schema = 'first_name:STRING,last_name:STRING' 
-    datetime_dim_schema = '''index:INTEGER, tpep_pickup_datetime:DATETIME, PU_day:INTEGER, PU_month:INTEGER, 
-                             PU_year:INTEGER, tpep_dropoff_datetime:DATETIME '''
+
     datetime_dim = data | 'CreateDateTimeDim' >> beam.Map(create_datetim_dim)
     
     datetime_dim | 'WriteToBigQuery' >> beam.io.WriteToBigQuery(
@@ -112,4 +153,3 @@ run_pipeline('nyc-taxi-project-423502', 'pipeline_test_pc', 'nyc_data', 'nyc_pip
 #   --staging_location gs://pipeline_test_pc/staging \
 #   --temp_location gs://pipeline_test_pc/temp \
 #   --runner DataflowRunner
-
